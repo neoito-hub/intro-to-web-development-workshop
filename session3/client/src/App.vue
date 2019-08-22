a2251e130ad57e0d2f7be6eb48f58a045bbbd008
aktsbot <box.ashishk@gmail.com>
2019-08-23 00:28:32 +0530

    got a skeleton going

--- session3/client/src/App.vue
+++ session3/client/src/App.vue
@@ -1,22 +1,39 @@
 <template>
   <div id="app" class="container">
-    <RecipeList msg="Welcome to Your Vue.js App"/>
+    <h1>Recipe App</h1>
+    <div class="add">
+      <button>Add New</button>
+    </div>
+
+    <div class="row">
+      <div class="col list">
+        <RecipeList :recipes="allRecipes"/>
+      </div>
+    </div>
+    
+    <div class="col selected">
+      <h2 style="margin-left: 10px;">Change Me</h2>
+      <SelectedRecipe />
+    </div>
+
   </div>
 </template>
 
 <script>
 import RecipeList from './components/RecipeList.vue'
+import SelectedRecipe from './components/SelectedRecipe.vue'
 
 import { get } from './api';
 
 export default {
   name: 'app',
   components: {
-    RecipeList
+    RecipeList,
+    SelectedRecipe
   },
   data() {
     return {
-      recipes: []
+      allRecipes: []
     }
   },
   async mounted() {
@@ -25,7 +42,7 @@ export default {
   methods: {
     async getAllRecipes() {
       try {
-        this.recipes = await get('/recipes')
+        this.allRecipes = await get('/recipes')
       } catch(e) {
         // console.log(e);
       }
---
+++ session3/client/src/components/SelectedRecipe.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <form action="javascript:void(0);">
+      <label for="recipe-name">Name</label>
+      <br/>
+      <input type="text" />
+
+      <br />
+      <br />
+
+      <label for="recipe-steps">Steps</label><br/>
+      <textarea name="recipe-steps" cols="35" rows="10">
+      </textarea>
+      
+      <br />
+      <br />
+
+      <button>Update</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SelectedRecipe'
+}
+</script>
+
+<style scoped>
+
+</style>
